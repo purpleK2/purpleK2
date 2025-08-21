1642862b8ba8a8c76cba260bc9884cd53a0739b1
RepubblicaTech <76620155+RepubblicaTech@users.noreply.github.com>
2025-08-21 23:01:47 +0200

    move register struct to a more proper location

--- src/arch/x86_64/cpu.h
+++ src/arch/x86_64/cpu.h
@@ -6,6 +6,8 @@
 #include <stddef.h>
 #include <stdint.h>
 
+#include <util/macro.h>
+
 // Vendor strings from CPUs.
 #define CPUID_VENDOR_AMD "AuthenticAMD"
 #define CPUID_VENDOR_AMD_OLD                                                   \
@@ -104,6 +106,39 @@
 #define CPUID_FEAT_EDX_TM      1 << 29
 #define CPUID_FEAT_EDX_IA64    1 << 30
 #define CPUID_FEAT_EDX_PBE     1 << 31
+
+typedef struct {
+    uint64_t ds;
+
+    uint64_t r15;
+    uint64_t r14;
+    uint64_t r13;
+    uint64_t r12;
+    uint64_t r11;
+    uint64_t r10;
+    uint64_t r9;
+    uint64_t r8;
+
+    uint64_t rbp;
+
+    uint64_t rdi;
+    uint64_t rsi;
+
+    uint64_t rdx;
+    uint64_t rcx;
+    uint64_t rbx;
+    uint64_t rax;
+
+    uint64_t interrupt;
+    uint64_t error;
+
+    uint64_t rip;
+    uint64_t cs;
+    uint64_t rflags;
+    uint64_t rsp;
+    uint64_t ss;
+
+} PACKED registers_t;
 
 // gets a value from a CRX register
 // by
