370355a46f6b9998d9a9f073919048b706ad62a7
NotNekodev <raphael.wegmann81@gmail.com>
2025-09-17 22:03:01 +0200

    more symbol stuff + updated submodules

--- src/kernel/elf/sym.h
+++ src/kernel/elf/sym.h
@@ -8,12 +8,10 @@
 
 const char *resolve_symbol_name(void *elf_data, size_t size, uint64_t addr);
 
-uint64_t
-resolve_symbol_addr(void *elf_data, size_t size,
-                    const char *symbol_name); // so this is like for kernel
-                                              // modules / drivers because :3c
+uint64_t resolve_symbol_addr(void *elf_data, size_t size,
+                             const char *symbol_name);
 
-uint64_t resolve_module_symbol(void *elf_data, Elf64_Shdr *shdrs,
-                               const char *symbol_name, uint8_t *load_base);
+uint64_t resolve_module_symbol(Elf64_Ehdr *ehdr, const char *symbol_name,
+                               uint8_t *load_base);
 
 #endif // ELF_SYM_H
