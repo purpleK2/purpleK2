cf6a2b2bd78a5a4bb7f4db5ab78ce9a08ea273f1
NotNekodev <raphael.wegmann81@gmail.com>
2025-09-17 22:03:01 +0200

    more symbol stuff + updated submodules

--- src/kernel/elf/sym.c
+++ src/kernel/elf/sym.c
@@ -1,6 +1,5 @@
 #include "sym.h"
 #include "stdio.h"
-
 #include <string.h>
 
 const char *resolve_symbol_name(void *elf_data, size_t size, uint64_t addr) {
@@ -14,8 +13,8 @@ const char *resolve_symbol_name(void *elf_data, size_t size, uint64_t addr) {
         return 0;
 
     Elf64_Shdr *shdrs = (Elf64_Shdr *)((uint8_t *)elf_data + ehdr->e_shoff);
-    Elf64_Shdr *shstrtab_hdr = &shdrs[ehdr->e_shstrndx];
-    const char *shstrtab     = (const char *)elf_data + shstrtab_hdr->sh_offset;
+    const char *shstrtab =
+        (const char *)elf_data + shdrs[ehdr->e_shstrndx].sh_offset;
 
     Elf64_Shdr *symtab_hdr = 0;
     Elf64_Shdr *strtab_hdr = 0;
@@ -39,8 +38,7 @@ const char *resolve_symbol_name(void *elf_data, size_t size, uint64_t addr) {
 
     for (uint64_t i = 0; i < num_syms; i++) {
         Elf64_Sym *sym = &symbols[i];
-        uint8_t type   = sym->st_info & 0x0F;
-        if (type == STT_FUNC && addr >= sym->st_value &&
+        if ((sym->st_info & 0x0F) == STT_FUNC && addr >= sym->st_value &&
             addr < sym->st_value + sym->st_size) {
             return strtab + sym->st_name;
         }
@@ -61,8 +59,8 @@ uint64_t resolve_symbol_addr(void *elf_data, size_t size,
         return 0;
 
     Elf64_Shdr *shdrs = (Elf64_Shdr *)((uint8_t *)elf_data + ehdr->e_shoff);
-    Elf64_Shdr *shstrtab_hdr = &shdrs[ehdr->e_shstrndx];
-    const char *shstrtab     = (const char *)elf_data + shstrtab_hdr->sh_offset;
+    const char *shstrtab =
+        (const char *)elf_data + shdrs[ehdr->e_shstrndx].sh_offset;
 
     Elf64_Shdr *symtab_hdr = NULL;
     Elf64_Shdr *strtab_hdr = NULL;
@@ -75,7 +73,7 @@ uint64_t resolve_symbol_addr(void *elf_data, size_t size,
             (!symtab_hdr || strcmp(name, ".symtab") == 0 ||
              strcmp(name, ".dynsym") == 0))
             symtab_hdr = &shdrs[i];
-        else if ((shdrs[i].sh_type == SHT_STRTAB) &&
+        else if (shdrs[i].sh_type == SHT_STRTAB &&
                  (strcmp(name, ".strtab") == 0 || strcmp(name, ".dynstr") == 0))
             strtab_hdr = &shdrs[i];
     }
@@ -104,20 +102,17 @@ uint64_t resolve_symbol_addr(void *elf_data, size_t size,
         if (sym->st_shndx == SHN_UNDEF)
             continue;
 
-        Elf64_Shdr *sec = &shdrs[sym->st_shndx];
-        return sym->st_value + sec->sh_addr;
+        return sym->st_value + shdrs[sym->st_shndx].sh_addr;
     }
 
     return 0;
 }
 
-uint64_t resolve_module_symbol(void *elf_data, Elf64_Shdr *shdrss,
-                               const char *symbol_name, uint8_t *load_base) {
-    UNUSED(shdrss);
-    Elf64_Ehdr *ehdr  = (Elf64_Ehdr *)elf_data;
-    Elf64_Shdr *shdrs = (Elf64_Shdr *)((uint8_t *)elf_data + ehdr->e_shoff);
-    Elf64_Shdr *shstrtab_hdr = &shdrs[ehdr->e_shstrndx];
-    const char *shstrtab     = (const char *)elf_data + shstrtab_hdr->sh_offset;
+uint64_t resolve_module_symbol(Elf64_Ehdr *ehdr, const char *symbol_name,
+                               uint8_t *load_base) {
+    Elf64_Shdr *shdrs = (Elf64_Shdr *)((uint8_t *)ehdr + ehdr->e_shoff);
+    const char *shstrtab =
+        (const char *)ehdr + shdrs[ehdr->e_shstrndx].sh_offset;
 
     Elf64_Shdr *symtab_hdr = NULL;
     Elf64_Shdr *strtab_hdr = NULL;
@@ -133,10 +128,9 @@ uint64_t resolve_module_symbol(void *elf_data, Elf64_Shdr *shdrss,
     if (!symtab_hdr || !strtab_hdr)
         return 0;
 
-    Elf64_Sym *symbols =
-        (Elf64_Sym *)((uint8_t *)elf_data + symtab_hdr->sh_offset);
+    Elf64_Sym *symbols = (Elf64_Sym *)((uint8_t *)ehdr + symtab_hdr->sh_offset);
     size_t num_syms    = symtab_hdr->sh_size / sizeof(Elf64_Sym);
-    const char *strtab = (const char *)elf_data + strtab_hdr->sh_offset;
+    const char *strtab = (const char *)ehdr + strtab_hdr->sh_offset;
 
     for (size_t i = 0; i < num_syms; i++) {
         Elf64_Sym *sym = &symbols[i];
