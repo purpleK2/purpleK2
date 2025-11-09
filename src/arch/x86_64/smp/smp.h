cda761d93fd9c99bd4ca4b073d78d6ef5644cdbf
RepubblicaTech <76620155+RepubblicaTech@users.noreply.github.com>
2025-11-09 15:57:11 +0100

    limine: update to v10.3.0
    that also broke lots of stuff :P

--- src/arch/x86_64/smp/smp.h
+++ src/arch/x86_64/smp/smp.h
@@ -11,7 +11,7 @@ struct tlb_shootdown_event {
 extern struct tlb_shootdown_event **events;
 
 int smp_init();
-void mp_trampoline(struct limine_smp_info *cpu);
+void mp_trampoline(struct limine_mp_info *cpu);
 
 uint8_t get_cpu();
 
