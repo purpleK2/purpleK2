5a4045a7388d4cedf390325fd4ec015dface792e
NotNekodev <97458908+NotNekodev@users.noreply.github.com>
2025-11-10 17:58:49 +0100

    i think it doesnt free randomly anymore

--- src/arch/x86_64/paging/paging.c
+++ src/arch/x86_64/paging/paging.c
@@ -77,8 +77,9 @@ void pf_handler(void *ctx) {
     uint64_t pf_error_code = (uint64_t)regs->error;
 
     if (PG_IF(pf_error_code)) {
+        debugf_warn("Process %d killed!\n", get_current_pcb()->pid);
         proc_exit();
-        yield(); // tell scheduler to select another process
+        yield(); // tell scheduler to select another process  427b1
         return;
     }
 
