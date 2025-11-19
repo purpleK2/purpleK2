4b3637930d6bed18b985b3cbe1fa0bd2106ec1b7
NotNekodev <97458908+NotNekodev@users.noreply.github.com>
2025-11-19 19:28:15 +0100

    actually add new files

---
+++ src/kernel/dev/diskdev.c
@@ -0,0 +1,179 @@
+#include "diskdev.h"
+#include "dev/device.h"
+#include "errors.h"
+#include "memory/heap/kheap.h"
+#include <stdio.h>
+
+static int diskdev_read(struct device *dev, void *buffer, size_t size, size_t offset) {
+    disk_device_t *disk = (disk_device_t *)dev->data;
+    if (!disk || !disk->read) {
+        return -1;
+    }
+
+    uint32_t sector_size = disk->block_size;
+    uint64_t start_lba   = offset / sector_size;
+    uint32_t sector_count =
+        (size + sector_size - 1) / sector_size; // round up to nearest sector
+
+    return disk->read(disk, (uint8_t *)buffer, start_lba, sector_count);
+}
+
+static int diskdev_write(struct device *dev, const void *buffer, size_t size, size_t offset) {
+    disk_device_t *disk = (disk_device_t *)dev->data;
+    if (!disk || !disk->write) {
+        return -1;
+    }
+
+    uint32_t sector_size = disk->block_size;
+    uint64_t start_lba   = offset / sector_size;
+    uint32_t sector_count =
+        (size + sector_size - 1) / sector_size; // round up to nearest sector
+
+    return disk->write(disk, (const uint8_t *)buffer, start_lba, sector_count);
+}
+
+static int diskdev_ctl(struct device *dev, int request, void *arg) {
+    (void)request;
+    (void)arg;
+    debugf_warn("diskdev_ctl not implemented on disk device %s\n", dev->name);
+    return -ENOIMPL;
+}
+
+int register_disk_device(disk_device_t *disk) {
+    char prefix[8];
+    char final_name[32];
+
+    switch (disk->namespace) {
+    case DISK_NAMESPACE_PATA:
+        snprintf(prefix, sizeof(prefix), "hd");
+        break;
+    case DISK_NAMESPACE_MODERN:
+        snprintf(prefix, sizeof(prefix), "sd");
+        break;
+    case DISK_NAMESPACE_NVME:
+        snprintf(prefix, sizeof(prefix), "nd");
+        break;
+    case DISK_NAMESPACE_FLOPPY:
+        snprintf(prefix, sizeof(prefix), "fd");
+        break;
+    case DISK_NAMESPACE_OPTICAL:
+        snprintf(prefix, sizeof(prefix), "opt");
+        break;
+    default:
+        return EINVAL;
+    }
+
+    if (disk->namespace == DISK_NAMESPACE_NVME) {
+        snprintf(final_name, sizeof(final_name), "%s%dn%d",
+                 prefix,
+                 disk->nvmeid.controller,
+                 disk->nvmeid.namespace);
+    } else {
+        snprintf(final_name, sizeof(final_name), "%s%c",
+                 prefix,
+                 disk->id.letter);
+    }
+
+    if (disk->dev != NULL) {
+        debugf_warn("Disk device %s already registered!\n", final_name);
+        return -EUNFB;
+    }
+
+    device_t *dev = kmalloc(sizeof(device_t));
+    if (!dev) {
+        return -ENOMEM;
+    }
+
+    snprintf(dev->name, DEVICE_NAME_MAX, "%s", final_name);
+    dev->major        = 2; // block storage device
+    dev->minor        = 0; // not used currently
+    dev->type         = DEVICE_TYPE_BLOCK;
+    dev->read         = diskdev_read;  // to be set by the driver
+    dev->write        = diskdev_write;           // to be set by the driver
+    dev->ioctl        = diskdev_ctl;           // to be set by the driver
+    dev->data         = (void *)disk; // point to the disk device struct
+
+    return EOK;
+}
+
+disk_device_t *get_disk_by_id(disk_namespace_e ns, char id_letter) {
+    char prefix[8];
+    char final_name[32];
+
+    switch (ns) {
+    case DISK_NAMESPACE_PATA:
+        snprintf(prefix, sizeof(prefix), "hd");
+        break;
+    case DISK_NAMESPACE_MODERN:
+        snprintf(prefix, sizeof(prefix), "sd");
+        break;
+    case DISK_NAMESPACE_NVME:
+        snprintf(prefix, sizeof(prefix), "nd");
+        break;
+    case DISK_NAMESPACE_FLOPPY:
+        snprintf(prefix, sizeof(prefix), "fd");
+        break;
+    case DISK_NAMESPACE_OPTICAL:
+        snprintf(prefix, sizeof(prefix), "opt");
+        break;
+    default:
+        return NULL;
+    }
+
+    snprintf(final_name, sizeof(final_name), "%s%c",
+             prefix,
+             id_letter);
+
+    device_t *dev = get_device(final_name);
+    if (!dev) {
+        return NULL;
+    }
+
+    return (disk_device_t *)dev->data;
+}
+
+disk_device_t *get_disk_by_nvmeid(int controller, int namespace) {
+    char final_name[32];
+
+    snprintf(final_name, sizeof(final_name), "nd%dn%d",
+             controller,
+             namespace);
+
+    device_t *dev = get_device(final_name);
+    if (!dev) {
+        return NULL;
+    }
+
+    return (disk_device_t *)dev->data;
+}
+
+int unregister_disk_device(disk_namespace_e ns, char id_letter) {
+    char prefix[8];
+    char final_name[32];
+
+    switch (ns) {
+    case DISK_NAMESPACE_PATA:
+        snprintf(prefix, sizeof(prefix), "hd");
+        break;
+    case DISK_NAMESPACE_MODERN:
+        snprintf(prefix, sizeof(prefix), "sd");
+        break;
+    case DISK_NAMESPACE_NVME:
+        snprintf(prefix, sizeof(prefix), "nd");
+        break;
+    case DISK_NAMESPACE_FLOPPY:
+        snprintf(prefix, sizeof(prefix), "fd");
+        break;
+    case DISK_NAMESPACE_OPTICAL:
+        snprintf(prefix, sizeof(prefix), "opt");
+        break;
+    default:
+        return EINVAL;
+    }
+
+    snprintf(final_name, sizeof(final_name), "%s%c",
+             prefix,
+             id_letter);
+
+    return unregister_device(final_name);
+}
---
+++ src/kernel/dev/diskdev.h
@@ -0,0 +1,48 @@
+#include "dev/device.h"
+#include <stdint.h>
+#ifndef DISKDEV_H
+#define DISKDEV_H 1
+
+typedef enum disk_namespace {
+    DISK_NAMESPACE_INVALID = -1,
+    DISK_NAMESPACE_PATA = 0, // /dev/hdX
+    DISK_NAMESPACE_MODERN = 1, // /dev/sdX
+    DISK_NAMESPACE_NVME = 2, // /dev/ndXnY
+    DISK_NAMESPACE_FLOPPY = 3, // /dev/fdX
+    DISK_NAMESPACE_OPTICAL = 4, // /dev/optX (this is like CD-ROM, CD-RW, DVD, etc)
+} disk_namespace_e;
+
+typedef struct disk_device {
+    disk_namespace_e namespace;
+    union {
+        struct {
+            int controller;
+            int namespace; 
+            // i have no idea if this is correct, i dont know much about nvme drives but from what i read it should be
+        } nvmeid;
+
+        struct {
+            char letter;
+        } id;
+    };
+
+    // populated by the API, NOT BY THE DRIVER
+    device_t *dev;
+
+    uint32_t block_size;
+    uint64_t block_count;
+
+    int (*read)(struct disk_device *disk, uint8_t *buffer, uint64_t lba,
+                uint32_t sector_count);
+    int (*write)(struct disk_device *disk, const uint8_t *buffer, uint64_t lba,
+                 uint32_t sector_count);
+
+    
+} disk_device_t;
+ 
+int register_disk_device(disk_device_t *disk);
+disk_device_t *get_disk_by_id(disk_namespace_e ns, char id_letter);
+disk_device_t *get_disk_by_nvmeid(int controller, int namespace);
+int unregister_disk_device(disk_namespace_e ns, char id_letter);
+
+#endif // DISKDEV_H
