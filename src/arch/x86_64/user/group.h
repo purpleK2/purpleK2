38a226c10bc3346835a03738267e58db1dfec34c
Nekodev <97458908+NotNekodev@users.noreply.github.com>
2026-01-24 21:39:31 +0100

    user: initial user / group system

---
+++ src/arch/x86_64/user/group.h
@@ -0,0 +1,10 @@
+#ifndef GROUP_H
+#define GROUP_H 1 
+
+#include <autoconf.h>
+
+#define UID_INVALID ((uid_t)-1)
+
+#define MAX_GROUPS CONFIG_KERNEL_MAX_GROUPS
+
+#endif // GROUP_H
---
+++ src/arch/x86_64/user/user.h
@@ -0,0 +1,38 @@
+#ifndef USER_H
+#define USER_H
+
+#include "types.h"
+#include "user/group.h"
+
+#define UID_INVALID ((uid_t)-1)
+
+typedef struct user_cred {
+    uid_t uid;
+    uid_t euid;
+    uid_t suid;
+
+    gid_t gid;
+    gid_t egid;
+    gid_t sgid;
+
+    gid_t groups[MAX_GROUPS];
+    uint8_t ngroups;    
+} user_cred_t;
+
+static inline int is_root(const struct user_cred *cred) {
+    return cred->euid == 0;
+}
+
+static inline int in_group(const struct user_cred *cred, gid_t gid) {
+    if (cred->egid == gid)
+        return 1;
+
+    for (uint8_t i = 0; i < cred->ngroups; i++) {
+        if (cred->groups[i] == gid)
+            return 1;
+    }
+    return 0;
+}
+
+
+#endif // USER_H
