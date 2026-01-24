5e5be281bafaff766a7585813ea50e6abf63318c
Nekodev <97458908+NotNekodev@users.noreply.github.com>
2026-01-24 22:12:35 +0100

    user: add access.h

---
+++ src/kernel/user/access.h
@@ -0,0 +1,20 @@
+#ifndef ACCESS_H
+#define ACCESS_H
+
+#define R_OK 0x4
+#define W_OK 0x2
+#define X_OK 0x1
+
+#define S_IRUSR 0400
+#define S_IWUSR 0200
+#define S_IXUSR 0100
+
+#define S_IRGRP 0040
+#define S_IWGRP 0020
+#define S_IXGRP 0010
+
+#define S_IROTH 0004
+#define S_IWOTH 0002
+#define S_IXOTH 0001
+
+#endif // ACCESS_H
